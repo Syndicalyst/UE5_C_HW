10b44ff32f8e7a4243361c6f3d86c0cf224068bf
Syndicalyst <denisov.dev@ukr.net>
2024-02-03 18:36:40 +0200

    Homework 15 - data struct of characters

---
+++ UE5_HW_15/UE5_HW_15/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+enum charClass { warrior, mage, healer, vampire };
+
+struct Character {
+	int id;
+	float meleeAttack;
+	float rangedAttack;
+	charClass cl;
+};
+
+int strongestCharId(vector<Character> characters) {
+	Character strongestChar = characters[0];
+
+	for (Character hero: characters) {
+		if (strongestChar.meleeAttack + strongestChar.rangedAttack < hero.meleeAttack + hero.rangedAttack) {
+			strongestChar = hero;
+		}
+	}
+
+	return strongestChar.id;
+}
+
+vector<pair<charClass, Character>> strongestCharByClass(vector<Character> characters) {
+	vector<pair<charClass, Character>> champChars;
+
+	charClass classes[4] = { warrior, mage, healer, vampire };
+	for (int i = 0; i < 4; i++) {
+		Character strongestInClass {0, 0, 0, classes[i]};
+		for (Character ch: characters) {
+			if (ch.cl == classes[i]) {
+				if (strongestInClass.meleeAttack + strongestInClass.rangedAttack < ch.meleeAttack + ch.rangedAttack) {
+					strongestInClass = ch;
+				}
+			}
+		}
+		champChars.push_back(make_pair(strongestInClass.cl, strongestInClass));
+	}
+
+	return champChars;
+}
+
+int main() {
+
+	vector<Character> heroes = { 
+		{1, 23, 0, charClass::warrior},  {2, 16, 0, charClass::warrior}, 
+		{3, 2, 30, charClass::mage}, {4, 2, 20, charClass::mage}, 
+		{5, 5, 5, charClass::healer}, {6, 5, 10, charClass::healer}, 
+		{7, 15, 12, charClass::vampire},   {8, 15, 11, charClass::vampire} };
+
+	cout << "Strongest Character id is: " << strongestCharId(heroes) << endl;
+
+	vector<pair<charClass, Character>> champChars = strongestCharByClass(heroes);
+
+	for (pair<charClass, Character> ch: champChars) {
+		string charClass;
+
+		if (ch.first == 0) charClass = "warrior";
+		if (ch.first == 1) charClass = "mage";
+		if (ch.first == 2) charClass = "healer";
+		if (ch.first == 3) { charClass = "vampire"; }
+
+		cout << "Strongest " << charClass << " id is " << ch.second.id << endl;
+	}
+
+
+	return 0;
+}
